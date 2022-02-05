baa924033df324eed1f7205806c1253cb045abcf
airman-dotcom <amathakbari@gmail.com>
2022-02-04 19:08:31 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,117 @@
+const express = require("express")
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const fs = require("fs");
+const { Server } = require("socket.io");
+const io = new Server(server);
+const { v4: uuidv4 } = require("uuid")
+app.use(express.static("public"));
+app.use(express.json());
+let users = {};
+let num = 1;
+let info;
+let user1;
+server.listen(3000, "localhost", () => {
+    console.log("Listening on http://localhost:3000/")
+})
+
+io.on("connection", (socket) => {
+    socket.on("Connected", () => {
+        socket.emit("Connected User", "A user has connected")
+    });
+    socket.on("Disconnected", () => {
+        socket.emit("Disconnected User", "A user has disconnected")
+    });
+    socket.on("join_room", (room) => {
+        socket.join(room);
+    });
+    /*socket.on("message", (room, message, user) => {
+        console.log(message);
+        const data = fs.readFileSync("cred.txt", "utf-8");
+        if (data == ""){
+            let message2 = "In Room: " + `"${room}"` + ". User: " + `"${user}"` + ", texted this: " + `"${message}"` + ".";
+            fs.writeFileSync("cred.txt", message2)
+        } else {
+            let message2 = data + "\nIn Room: " + `"${room}"` + ". User: " + `"${user}"` + ", texted this: " + `"${message}"` + ".";
+            fs.writeFileSync("cred.txt", message2);
+        }
+        io.to(room).emit("message2", message, (user))
+    });*/
+    socket.on("join_video", (room_name) => {
+        socket.join(room_name);
+    });
+    socket.on("data", (room, data) => {
+        socket.to(room).emit("video", data)
+    })
+    socket.on("name", (nam, id) => {
+      if (JSON.stringify(users) == "{}"){
+        users = `{"${nam}": "${id}"}`;
+        users = JSON.parse(users)
+      } else {
+        users = JSON.stringify(users);
+        users = users.slice(0, -1) + `,"${nam}": "${id}"}`;
+        users = JSON.parse(users)
+      }
+    });
+    socket.on("get_id", (data) => {
+      user1 = Object.values(data)[0]//User1 Id
+      let user1_index = Object.values(users).indexOf(user1);
+      let user1_name = Object.keys(users)[user1_index]
+      let to = Object.values(data)[1];
+      if (Object.keys(users).includes(to)){
+        let index = Object.keys(users).indexOf(to);
+        let user2_id = Object.values(users)[index];
+        
+        socket.broadcast.to(user2_id).emit("join", (user1_name));
+      }
+    })
+    socket.on("join2", (answer) => {
+      if (answer == "y"){
+        socket.broadcast.to(user1).emit("answer", "y")
+      }
+       else if (answer == "n"){
+        socket.broadcast.to(user1).emit("answer", "n")
+      } else {
+        console.log("err");
+      }
+    });
+    
+    socket.on("message", (data) => {
+      let message = Object.values(data)[0]
+      let sender = Object.values(data)[1];
+      let sender_id;
+      let receiver_id;
+      let receiver = Object.values(data)[2];
+      if (Object.keys(users).includes(sender) && Object.keys(users).includes(receiver)){
+        sender_id = Object.values(users)[Object.keys(users).indexOf(sender)];
+        receiver_id = Object.values(users)[Object.keys(users).indexOf(receiver)];
+        io.to(sender_id).to(receiver_id).emit("message2", data);
+      }
+      
+    })
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/video", (req, res) => {
+    res.sendFile(__dirname + "/public/video.html")
+})
+
+app.post("/code", (req, res) => {
+    const code = uuidv4();
+    res.json({code: code})
+})
+
+app.post("/stream", (req, res) => {
+    let data = req.body.data;
+    let send = Readable.from(data)
+    res.json({data: send})
+})
+
+app.post("/test", (req, res) => {
+  console.log("disconnected")
+})
+
---
+++ public/scripts/video.js
@@ -0,0 +1,238 @@
+let text = document.querySelector("p");
+const socket = io();
+let screen = window.getComputedStyle(document.body)
+let text_input = document.getElementById("message");
+let send_btn = document.getElementById("send");
+let screen_num = parseInt(screen.getPropertyValue("width").slice(0, -2))
+let my_video = document.getElementById("v_1");
+let your_video = document.getElementById("v_2");
+let room_input = document.getElementById("code");
+const join_room = document.getElementById("join");
+let variable;
+let v;
+alert(navigator.platform)
+alert(parseInt(screen.getPropertyValue("width").slice(0, -2)))
+let keys = [];
+let user = prompt("What is your name");
+while (user == "" || user == null || user == undefined){
+  user = prompt("What is your name")
+}
+let connected_users;
+let con;
+let yes_no = false;
+var peer;
+let num = 0;
+let stream2;
+let user_two;
+
+
+function testName(){
+  if (localStorage.getItem(user) == null || localStorage.getItem(user) == undefined || localStorage.getItem(user) == ""){
+    localStorage.setItem(user, socket.id);
+    connected_users = {me: user, you: undefined};
+    socket.emit("name", user, (socket.id))
+  } else {
+    alert("That name already exists, choose a different one");
+    user = prompt("What is your name");
+      while (user == "" || user == undefined || user == null){
+        user = prompt("What is your name");
+    }
+    testName();
+  }
+}
+
+let handleSuccess = function(stream){
+    stream2 = stream;
+    join_room.addEventListener("click", () =>{
+      if (room_input.value != "" || room_input.value != null || room_input.value != undefined){
+        
+        socket.emit("get_id", {"me": socket.id, "to": room_input.value})
+        const call = peer.call(room_input.value, stream);
+        call.on("stream", function(remoteStream){
+          connected_users = {me: user, you: room_input.value}
+          your_video.srcObject = remoteStream;
+          
+        })
+      }
+    })
+    
+    peer.on("call", function(call){
+      window.focus();
+      setInterval(function(){
+        if (con){
+          call.answer(stream2);
+        }
+      })
+      
+      call.on("stream", function(otherStream){
+        your_video.srcObject = otherStream;
+      })
+    })
+    
+    my_video.srcObject = stream2;
+    
+}
+
+
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+.then(handleSuccess)
+
+socket.on("video", (data) => {
+    const data1 = {
+        data: data
+    };
+    const send_data = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data1)
+    };
+    fetch("/stream", send_data)
+    .then(response => response.json())
+    .then(function(json){
+        stream2 = Object.values(json)[0]
+    })
+    console.log(1)
+    your_video.srcObject = stream2;
+})
+
+
+socket.on("connect", () => {
+  testName();
+})
+
+window.oncontextmenu = function(){
+  if (document.body != null){
+    alert("Hey hey hey! What are you trying to pull?");
+    localStorage.removeItem(user)
+    document.body.remove();
+    window.location.href = "https://greater.armanakbari1.repl.co"
+  }
+}
+
+
+window.addEventListener("beforeunload", (e) => {
+  e.returnValue = "Test";
+  localStorage.removeItem(user);
+})/*
+document.addEventListener("keydown", (e) => {
+  
+  if (keys.length < 3){
+    keys.push(e.keyCode);
+    console.log(keys)
+  } if (keys.length == 3){
+    console.log(keys)
+    if (keys.includes(17) && keys.includes(16) && keys.includes(73)){
+      localStorage.removeItem(user);
+      alert("Hey hey hey! What are you trying to pull?");
+      document.body.remove();
+      window.location.href = "https://greater.armanakbari1.repl.co";
+    } else if (keys.includes(17) && keys.includes(16) && keys.includes(74)){
+      localStorage.removeItem(user);
+      alert("Hey hey hey! What are you trying to pull?");
+      document.body.remove();
+      window.location.href = "https://greater.armanakbari1.repl.co";
+    } else if (keys.includes(17) && keys.includes(16) && keys.includes(67)){
+      localStorage.removeItem(user);
+      alert("Hey hey hey! What are you trying to pull?");
+      document.body.remove();
+      window.location.href = "https://greater.armanakbari1.repl.co";
+    } else {
+      keys = [];
+    }
+  }
+    if (keys.length == 1){
+      if (keys[0] == 123){
+      localStorage.removeItem(user);
+      alert("Hey hey hey! What are you trying to pull?");
+      document.body.remove();
+      window.location.href = "https://greater.armanakbari1.repl.co";
+      }
+    }
+    if (keys.length == 2){
+      if (keys.includes(17) && keys.includes(85)){
+        localStorage.removeItem(user);
+      alert("Hey hey hey! What are you trying to pull?");
+      document.body.remove();
+      window.location.href = "https://greater.armanakbari1.repl.co";
+      }
+    }
+})*/
+
+document.body.onload = function(){
+  /*if ((navigator.platform).slice(0, -2) == "Win" && screen_num < 1200){
+    localStorage.removeItem(user);
+    alert("Hey hey hey! What are you trying to pull?")
+    
+    document.body.remove();
+    window.location.href = "https://greater.armanakbari1.repl.co"
+  }*/
+  peer = new Peer(user)
+}
+
+socket.on("join", (user2) => {
+  text.innerHTML = "A user would like to call. Accept call? (y/n)";
+  yes_no = true;
+  console.log(user2)
+  user_two = user2;
+})
+
+send_btn.addEventListener("click", () => {
+  if (yes_no){
+    if (text_input.value == "y"){
+      con = true;
+      connected_users = {me: user, you: user_two};
+      user_two = undefined;
+      socket.emit("join2", "y")
+      text_input.value = "";
+      text.innerHTML = "";
+      yes_no = false;
+    }
+    if (text_input.value == "n"){
+      con = false;
+      usertwo = undefined;
+      socket.emit("join2", "n")
+      alert("Call declined");
+      text_input.value = "";
+      text.innerHTML = "";
+      yes_no = false;
+    }
+  } else if (!yes_no){
+    if (Object.values(connected_users)[1] != undefined){
+      console.log("Message sending");
+      socket.emit("message", {message: text_input.value, frome: user, to: Object.values(connected_users)[1]});
+    }
+  }
+})
+
+socket.on("answer", (answer) => {
+  if (answer == "y"){
+    alert("Call accepted");
+  } else if(answer == "n"){
+    alert("Call declined")
+  }
+})
+
+socket.on("message2", (data) => {
+  let message = Object.values(data)[0];
+  let sender = Object.values(data)[1];
+  let receiver = Object.values(data)[2];
+  console.log("sender: " + sender);
+  console.log("receiver: " + receiver)
+  if (user == sender){
+    if (text.innerHTML == ""){
+      text.innerHTML = `Me: ${message}`;
+    } else {
+      text.innerHTML += `<br>Me: ${message}`;
+    }
+  } else if (user == receiver){
+    console.log(receiver);
+    console.log(text.innerHTML == "")
+    if (text.innerHTML == ""){
+      text.innerHTML = `${sender}: ${message}`;
+    } else {
+      text.innerHTML += `<br>${sender}: ${message}`;
+    }
+  }
+})
